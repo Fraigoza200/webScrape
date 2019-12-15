1f5922bb7af46aa871cd026344c03f9be62c5050
Francisco Raigoza <raigoza.frankie@gmail.com>
2019-12-15 12:55:45 -0800

    changed app to server

---
+++ server.js
@@ -0,0 +1,13 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+require('mongoose')
+  .connection
+  .once('open', () => app.listen(3000))
